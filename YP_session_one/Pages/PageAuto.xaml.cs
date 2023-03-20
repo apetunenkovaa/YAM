6a1763b6b5e4a1d42bc4508468f4b1c6f1ae2fda
apetunenkovaa <apetunenkovaa@mail.ru>
2023-03-21 02:32:28 +0300

    commit

--- YP_session_one/Pages/PageAuto.xaml.cs
+++ YP_session_one/Pages/PageAuto.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace YP_session_one.Pages
 {
@@ -20,10 +21,64 @@ namespace YP_session_one.Pages
     /// </summary>
     public partial class PageAuto 
     {
+        WindowsKod windowsKod = new WindowsKod();
+        public int kk2 = 0;
+        private int timer = 10;
+        public int kk;
+        public int v;
+        public int v2;
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        string kod;
+        string kod1;
+        bool ss = false;
+
         public PageAuto()
         {
             InitializeComponent();
         }
+
+        public PageAuto(int kk, string kod)
+        {
+            InitializeComponent();
+            if (kk != 0)
+            {
+                Kod.IsEnabled = true;
+                Password.IsEnabled = false;
+                Number.IsEnabled = false;
+
+                Kod.Focus();
+
+                dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+                dispatcherTimer.Tick += new EventHandler(Backk);
+                dispatcherTimer.Start();
+                kk2 = kk;
+                kod1 = kod;
+                v2 = v;
+            }
+        }
+
+
+        public PageAuto(int kk, string kod, int v)
+        {
+            InitializeComponent();
+            if (kk != 0)
+            {
+                Kod.IsEnabled = true;
+                Password.IsEnabled = false;
+                Number.IsEnabled = false;
+
+                Kod.Focus();
+
+                dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+                dispatcherTimer.Tick += new EventHandler(Backk);
+                dispatcherTimer.Start();
+                kk2 = kk;
+                kod1 = kod;
+                v2 = v;
+                v2++;
+            }
+        }
+
 
         private void Number_KeyDown(object sender, KeyEventArgs e)
         {
@@ -45,6 +100,27 @@ namespace YP_session_one.Pages
             }
         }
 
+        private void Backk(object sender, EventArgs e)
+        {
+            if (timer == -1)
+            {
+                dispatcherTimer.Stop();
+                TextClue.Text = "Вы не успели ввести код\nДля повторной отправки кода нажмите кнопку";
+                Kod.IsEnabled = false;
+                Password.IsEnabled = false;
+                Number.IsEnabled = false;
+                Kod.Text = "";
+                img.IsEnabled = true;
+                Entry.IsEnabled = false;
+
+            }
+            else
+            {
+                TextClue.Text = "Таймер: " + timer;
+                Entry.IsEnabled = true;
+            }
+            timer--;
+        }
         private void Password_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
@@ -54,11 +130,107 @@ namespace YP_session_one.Pages
 
                 if (employees != null)
                 {
-                    
+                    windowsKod.ShowDialog();
+                    kk = employees.Role;
+                    kod = windowsKod.text;
+
+                   ClassFrame.lframe.Navigate(new PageAuto(kk, kod));
+                    dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+                    dispatcherTimer.Tick += new EventHandler(Backk);
+                    dispatcherTimer.Start();
                 }
                 else
                 {
                     MessageBox.Show("Пароль введен неверно", "Сообщение");
+                }
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Number.Clear();
+            Password.Clear();
+            Kod.Clear();
+            Kod.IsEnabled = false;
+            Password.IsEnabled = false;
+            Number.IsEnabled = true;
+            img.IsEnabled = false;
+            dispatcherTimer.Stop();
+            TextClue.Text = "";
+        }
+
+        private void Entry_Click(object sender, RoutedEventArgs e)
+        {
+            Role roles = ClassBase.BD.Role.FirstOrDefault(x => x.Role_ID == kk2);
+            if (Kod.Text == kod1)
+            {
+                MessageBox.Show($"Поздравляю вы авторизировались!\nВаша роль {roles.Role1}", "Сообщение");
+                Kod.IsEnabled = false;
+                Password.IsEnabled = false;
+                Number.IsEnabled = false;
+                img.IsEnabled = false;
+                dispatcherTimer.Stop();
+                TextClue.Text = "";
+                Kod.Text = "";
+            }
+            else
+            {
+                Password.IsEnabled = false;
+                Number.IsEnabled = false;
+                MessageBox.Show("Неверный код", "Сообщение");
+            }
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (v2 != 1)
+            {
+                WindowsKod windowKod = new WindowsKod();
+                windowKod.ShowDialog();
+                v = windowKod.v;
+                //kk = employees.id_role;
+                kod = windowKod.text;
+                ClassFrame.lframe.Navigate(new PageAuto(kk2, kod, v2));
+
+                dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+                dispatcherTimer.Tick += new EventHandler(Backk);
+                dispatcherTimer.Start();
+            }
+            else
+            {//uyv=X)%k
+                MessageBox.Show("Закончились попытки", "Сообщение");
+                Kod.IsEnabled = false;
+
+                Back.IsEnabled = false;
+                Entry.IsEnabled = false;
+            }
+        }
+
+        private void Kod_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Role roles = ClassBase.BD.Role.FirstOrDefault(x => x.Role_ID == kk2);
+                //MessageBox.Show($"{kod1}", "Сообщение");
+                if (Kod.Text == kod1)
+                {
+                    MessageBox.Show($"Поздравляю вы авторизировались!\nВаша роль {roles.Role1}", "Сообщение");
+                    Kod.IsEnabled = false;
+                    Password.IsEnabled = false;
+                    Number.IsEnabled = false;
+                    Back.IsEnabled = false;
+                    Entry.IsEnabled = false;
+                    img.IsEnabled = false;
+                    dispatcherTimer.Stop();
+                    TextClue.Text = "";
+                    Kod.Text = "";
+                }
+                else
+                {
+                    Password.IsEnabled = false;
+                    Number.IsEnabled = false;
+                    Entry.IsEnabled = false;
+                    MessageBox.Show("Неверный код", "Сообщение");
                 }
             }
         }
--- YP_session_one/WindowsKod.xaml.cs
+++ YP_session_one/WindowsKod.xaml.cs
@@ -19,9 +19,93 @@ namespace YP_session_one
     /// </summary>
     public partial class WindowsKod : Window
     {
+        public string text = String.Empty;
+        public int v = 0;
+
         public WindowsKod()
         {
             InitializeComponent();
+            Random rnd = new Random();
+            for (int i = 0; i < 15; i++)
+            {
+                SolidColorBrush brush = new SolidColorBrush(Color.FromRgb((byte)rnd.Next(256), (byte)rnd.Next(256), (byte)rnd.Next(256)));
+                Line line = new Line()
+                {
+                    X1 = rnd.Next((int)canvas.Width),
+                    Y1 = rnd.Next((int)canvas.Height),
+                    X2 = rnd.Next((int)canvas.Width),
+                    Y2 = rnd.Next((int)canvas.Height),
+                    Stroke = brush,
+                };
+                canvas.Children.Add(line);
+            }
+
+
+            text = String.Empty;
+            string chars = "abcdefghijklmnopqrstuvwxyzQWERTYUIOPASDFGHJKLZXCVBNM0123456789[]!@#$%^&*()_-=+№;:?";
+            for (int i = 0; i < 8; i++)
+            {
+                text += chars[rnd.Next(chars.Length)];
+
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                int margin = 40;
+                int v = rnd.Next(3);
+                if (v == 0)
+                {
+                    int font = rnd.Next(16, 20);
+
+                    TextBlock textBlock = new TextBlock()
+                    {
+                        Text = text[i].ToString(),
+                        FontSize = font,
+                        FontStyle = FontStyles.Italic,
+                        Margin = new Thickness(i * 35, rnd.Next(50), rnd.Next(50), 0),
+
+                    };
+                    canvas.Children.Add(textBlock);
+                }
+                else if (v == 1)
+                {
+                    int font = rnd.Next(16, 25);
+
+                    TextBlock textBlock = new TextBlock()
+                    {
+                        Text = text[i].ToString(),
+                        FontSize = font,
+
+                        Margin = new Thickness(i * 35, rnd.Next(50), rnd.Next(50), 0),
+
+                    };
+                    canvas.Children.Add(textBlock);
+                }
+                else if (v == 2)
+                {
+                    int font = rnd.Next(16, 25);
+
+                    TextBlock textBlock = new TextBlock()
+                    {
+                        Text = text[i].ToString(),
+                        FontSize = font,
+                        FontWeight = FontWeights.Bold,
+                        FontStyle = FontStyles.Italic,
+                        Margin = new Thickness(i * 35, rnd.Next(50), rnd.Next(50), 0),
+
+                    };
+                    canvas.Children.Add(textBlock);
+                }
+
+            }
+
+        }
+
+        private void buttonCaptcha_Click(object sender, RoutedEventArgs e)
+        {
+            v = 1;
+            //MessageBox.Show($"{text}", "Сообщение");
+            this.Close();
         }
     }
 }
